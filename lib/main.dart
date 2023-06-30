527dc11d886e99de0d41351755c9d2bbc9ea4beb
Adii <77727680+AdityaMotale@users.noreply.github.com>
2023-06-30 12:47:32 +0530

    prototype with valueNotifier

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'stream/stream_test.dart';
+import 'value_notifier/value_test.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const StreamCounter(),
+      home: const ValueCounter(),
     );
   }
 }
