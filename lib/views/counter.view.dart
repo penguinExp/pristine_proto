b2dd4bb293275ade10e5f483de643ce1322f19fb
Adii <77727680+AdityaMotale@users.noreply.github.com>
2023-10-08 23:30:00 +0530

    added docs in pristine controller and counterController (example)

--- lib/views/counter.view.dart
+++ lib/views/counter.view.dart
@@ -19,11 +19,13 @@ class _CounterViewState extends State<CounterView> {
   void initState() {
     super.initState();
 
+    // init the controller
     controller = Pristine().put(() => CounterController());
   }
 
   @override
   void dispose() {
+    // TODO: Need to test by manually deleting the controller
     Pristine().delete<CounterController>();
 
     super.dispose();
@@ -80,21 +82,32 @@ class CounterController extends PristineController {
   void onInit() {
     super.onInit();
 
+    // Counter-1 depends on counter-2's value,
+    // when counter-2 updates it will update automatically
+    // the `d` represents how it'll be updated
     counter1 = Store(0, d: (val) {
       return val + counter2.state;
     });
 
+    // when this controller will update then its dependencies will automatically will be updated
     counter2 = Store(0, dependencies: {counter1});
   }
 
   @override
   void onDispose() {
+    // user manually needs to dispose off the controllers
     counter1.dispose();
     counter2.dispose();
     super.onDispose();
   }
 
+  // Can update the value of the controller depending on its current value
   void updateCounter2() {
     counter2.update((p0) => p0 + 1);
+  }
+
+  // Can also update the value of the controller directly
+  void resetCounter1() {
+    counter1.assign(0);
   }
 }
